5905da741c8a555941bce3ad246c21c95dc43607
abylayomar <abylay.omar@gmail.com>
2020-07-04 20:59:19 +0600

    Initial

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+const { divide } = require("lodash");
+
+<template>
+    <div>
+        <h1>Hello</h1>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<sciprt>
+    export default {
+        name: "App"
+    }
+</sciprt>
+
+<style scoped>
+
+</style>
